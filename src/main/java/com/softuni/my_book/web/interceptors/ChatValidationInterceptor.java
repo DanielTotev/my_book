8328c3a244d64d237f7a1dda4aad53e93ec5712b
DanielTotev <dtotev1@gmail.com>
2019-04-13 23:11:53 +0300

    Found bug at chat validation interceptor

--- src/main/java/com/softuni/my_book/web/interceptors/ChatValidationInterceptor.java
+++ src/main/java/com/softuni/my_book/web/interceptors/ChatValidationInterceptor.java
@@ -1,6 +1,7 @@
 package com.softuni.my_book.web.interceptors;
 
 import com.softuni.my_book.domain.models.service.UserServiceModel;
+import com.softuni.my_book.service.contracts.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,19 +14,23 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class ChatValidationInterceptor extends HandlerInterceptorAdapter {
     private final ModelMapper mapper;
+    private final UserService userService;
 
     @Autowired
-    public ChatValidationInterceptor(ModelMapper mapper) {
+    public ChatValidationInterceptor(ModelMapper mapper, UserService userService) {
         this.mapper = mapper;
+        this.userService = userService;
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String friendUsername = request.getQueryString().split("=")[1];
-        UserServiceModel user = this.mapper.map(SecurityContextHolder
+        String  username = this.mapper.map(SecurityContextHolder
                 .getContext()
                 .getAuthentication()
-                .getPrincipal(), UserServiceModel.class);
+                .getPrincipal(), UserServiceModel.class).getUsername();
+
+        UserServiceModel user = this.userService.findByUsername(username);
 
         if(user.getFriends().stream().noneMatch(u -> u.getUsername().equals(friendUsername))) {
             response.sendRedirect("/friends");
